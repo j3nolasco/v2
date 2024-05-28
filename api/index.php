5477bedfd14817f2d33f7a98a13543025847c493
j3nolasco <j3nolasco@nwacc.edu>
2024-05-28 09:24:51 -0500

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,7 +1,7 @@
 <?php
 
-$path =__DIR__ . '/Data.json';
-$jsonString = file_get_contents($path);
+//$path =__DIR__ . '/Data.json';
+$jsonString = file_get_contents('./Data.json', FILE_USE_INCLUDE_PATH);
 $data = json_decode($jsonString, true);
 $reportEntries = $data['Report_Entry'];
 
